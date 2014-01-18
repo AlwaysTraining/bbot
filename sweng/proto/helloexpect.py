c33667d4881e0b7a781f11463ea562cadea30486
Derrick Karimi <dhkarimi@sei.cmu.edu>
2014-01-17 21:32:57 -0500

    loggs in now

--- sweng/proto/helloexpect.py
+++ sweng/proto/helloexpect.py
@@ -13,9 +13,11 @@ switch_ip_address = "shenks.synchro.net"
 
 print 'ip address is: ', switch_ip_address
 
-telnet_child = pexpect.spawn('telnet ' + switch_ip_address)
-telnet_child.logfile = sys.stdout
+telnet_child = pexpect.spawn('telnet ' + switch_ip_address,logfile=sys.stdout)
+telnet_child.delaybeforesend=1
 telnet_child.expect('Login: ')
-telnet_child.sendline('Randy32')
+telnet_child.sendline('Randy32\r')
 telnet_child.expect('Password: ')
-telnet_child.sendline('RANDYPAS')
+telnet_child.sendline('RANDYPAS\r')
+telnet_child.expect_exact('[Hit a key]')
+telnet_child.sendline('\r')
