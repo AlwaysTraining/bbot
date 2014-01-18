455c1cfd42809eba84a97c433346b3248432f306
Derrick Karimi <dhkarimi@sei.cmu.edu>
2014-01-17 23:23:02 -0500

    thining about how to do a state based thing

--- sweng/proto/helloexpect.py
+++ sweng/proto/helloexpect.py
@@ -13,11 +13,23 @@ switch_ip_address = "shenks.synchro.net"
 
 print 'ip address is: ', switch_ip_address
 
-telnet_child = pexpect.spawn('telnet ' + switch_ip_address,logfile=sys.stdout)
-telnet_child.delaybeforesend=1
-telnet_child.expect('Login: ')
-telnet_child.sendline('Randy32\r')
-telnet_child.expect('Password: ')
-telnet_child.sendline('RANDYPAS\r')
-telnet_child.expect_exact('[Hit a key]')
-telnet_child.sendline('\r')
+t = pexpect.spawn('telnet ' + switch_ip_address,logfile=sys.stdout)
+t.delaybeforesend=1
+t.expect('Login: ')
+t.sendline('Randy32\r')
+t.expect('Password: ')
+t.sendline('RANDYPAS\r')
+t.expect_exact('[Hit a key]')
+t.sendline('\r')
+t.expect_exact('Enter number of bulletin to view or press (ENTER) to continue:')
+t.sendline('\r')
+t.expect_exact('Search all groups for new messages?')
+t.sendline('n')
+t.expect_exact('Search all groups for un-read messages to you')
+t.sendline('n')
+t.expect('AFTERSHOCK:')
+t.sendline('\/o')
+
+
+
+t.expect('zzzzzzsads232e1fd2312')
