d532cdeda05e7cf04687c6c47ac3667e55273817
Ubuntu <ubuntu@ip-172-31-33-42.ec2.internal>
2014-02-07 02:43:38 +0000

    latest cron stuff

--- bin/bbots.sh
+++ bin/bbots.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
 
+# BRE Bot Command Line Interface
+#
+#       positional arguments:
+#         strategies            list of actions to perform
+#
+#       optional arguments:
+#         -h, --help            show this help message and exit
+#         -u USERNAME, --username USERNAME
+#                               Your BBS username.
+#         -p PASSWORD, --password PASSWORD
+#                               Your BBS password.
+#         -a ADDRESS, --address ADDRESS
+#                               BBS address
+#         -r REALM, --realm REALM
+#                               name of realm
+#         -g GAME, --game GAME  menu number for the game
+#         --smtp-server SMTP_SERVER
+#                               Outgoing Mail Server address
+#         --smtp-port SMTP_PORT
+#                               Outgoing Mail Server port
+#         --smtp-user SMTP_USER
+#                               Outgoing Mail Server user name
+#         --smtp-password SMTP_PASSWORD
+#                               Outgoing Mail Server user password
+#         -n [NOTIFY], --notify [NOTIFY]
+#                               email addresses for recipiants of
+#                                                notifications
+#         -d, --debug           enable debug mode
 
 
-bbot-cli.py
+
+
+bbot-cli.py \
+        --username                Randy32 \
+        --password                RANDYPAS \
+        --address                tnsoa.strangled.net \
+        --realm                Randyland \
+        --game                10 \
+        --smtp-server        smtp.gmail.com \
+        --smtp-port        587 \
+        --smtp-user        dr.randy.myers \
+        --smtp-password        dr.randy.myers.password \
+        --notify                derrick.karimi@gmail.com
+        --strategies        IndMtn
--- bin/cron.sh
+++ bin/cron.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-echo "Starting bbots"
+echo "bbots cron job begins: $(date)"
+
+# Grab a random value between 0-3600
+value=$(($RANDOM%3600))
+# Sleep for that time.
+echo Sleeping for $(($RANDOM%3600/60)) minutes
+sleep $value
+
+echo "Starting bbots: $(date)"
+
+logger "Starting bbots"
+
 id="1FzFO8mnQDNYTRKiHYcLpoHOvqxZJClBVZLtVXyuteKI"
 url="https://docs.google.com/feeds/download/documents/export/Export?id=$id&exportFormat=txt"
-echo "Pulling Latest from source control"
+echo "Pulling Latest code from source control"
 cd $(dirname $0)
 git pull
 echo "Setting up python environment"
 source devenv.rc
+echo "Downloading latest commands from google docs"
 curl -s $url > bbots.sh
 dos2unix bbots.sh
 chmod +x bbots.sh
+echo "Adding latest file from google docs to source control"
+git add .
+git commit -m "Latest command file from google doc"
+git push
 ./bbots.sh
-echo "bbots Finished"
+logger "Finished bbots"
+echo "bbots Finished: $(date)"
