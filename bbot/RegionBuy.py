6492e61640867730ae807f69eeb96087c6fbe4b5
Derrick Karimi <derrick.karimi@gmail.com>
2014-07-27 21:57:58 -0400

    introduced a bug that caused revolts

--- bbot/RegionBuy.py
+++ bbot/RegionBuy.py
@@ -60,10 +60,10 @@ class RegionBuy(Strategy):
         # it has been shown necessary in war time, something with how the
         #   advisor calculates the deficit when there are a lot of waste
         #   regions
-        if (wartime or (
-                app.metadata.last_ag_buy_turn is not None and
+        if (wartime or 
+            app.metadata.last_ag_buy_turn is None or (
                 app.metadata.last_ag_buy_turn !=
-                        self.data.realm.turns.current)):
+                self.data.realm.turns.current)):
             # buy just enough ag, this sends us back to the spending menu
             self.buy_ag_regions()
             app.metadata.last_ag_buy_turn = self.data.realm.turns.current
@@ -91,7 +91,7 @@ class RegionBuy(Strategy):
             # if we are very low on regions, buy some
             if (self.data.realm.regions.number is not None and
                     self.data.realm.regions.number < 500):
-                self.a = int(min(self.a), 500)
+                self.a = int(min(self.a, 500))
 
             # during wartime we do not but regions unless in danger of bank
             # overflow, and then we will only buy 1/8th of what we can afford
