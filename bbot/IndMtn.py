b7c57c7cc151920b12d4fedf379c790220119558
Derrick Karimi <dhkarimi@sei.cmu.edu>
2014-07-28 23:16:07 -0400

    refactored ind mtn to a base class for reuse of main buy sell logic

--- bbot/IndMtn.py
+++ bbot/IndMtn.py
@@ -5,10 +5,8 @@
 # Unclassified
 
 from bbot.Utils import *
-from bbot.Strategy import Strategy
-from bbot.SpendingParser import SpendingParser
+from bbot.MainStrategy import MainStrategy
 from bbot.Data import *
-from bbot.RegionBuy import RegionBuy
 
 S = SPACE_REGEX
 N = NUM_REGEX
@@ -35,36 +33,11 @@ def get_region_ratio(app, context):
     return r
 
 
-class IndMtn(Strategy):
+class IndMtn(MainStrategy):
     def __init__(self, app):
-        Strategy.__init__(self, app)
-        self.data = self.app.data
+        MainStrategy.__init__(self, app)
         self.app.metadata.get_region_ratio_func = get_region_ratio
         self.app.metadata.get_region_ratio_context = self
-        self.sp = SpendingParser()
-        self.do_specialize = False
-        self.protection_sell_ratio = self.get_strategy_option(
-            "protection_sell_ratio")
-        self.investing_sell_ratio = self.get_strategy_option(
-            "investing_sell_ratio")
-        self.normal_sell_ratio = self.get_strategy_option(
-            "normal_sell_ratio")
-
-        self.protection_buy_ratio = self.get_strategy_option(
-            "protection_buy_ratio")
-        self.investing_buy_ratio = self.get_strategy_option(
-            "investing_buy_ratio")
-        self.normal_buy_ratio = self.get_strategy_option(
-            "normal_buy_ratio")
-        self.visited_sell_menu = False
-
-        if self.normal_sell_ratio * self.normal_buy_ratio != 0:
-            botlog.warn("buy ratio of " + str(self.normal_buy_ratio) +
-                " and sell ratio of " + str(self.normal_sell_ratio) +
-                " are self conflicting")
-
-    def get_priority(self):
-        return MED_PRIORITY
 
     def on_industry_menu(self):
 
@@ -86,172 +59,24 @@ class IndMtn(Strategy):
         self.app.sendl()
 
 
-    def get_army_sell_ratio(self):
+    def get_specialize_seq(self):
+        return ['*', 3, 5, 0]
 
-        # from the beginning of the game to the first
-        #   total day out of protection, we will always
-        #   sell 100% of regions
-        if not self.data.is_oop():
-            return self.protection_sell_ratio
+    def get_buy_unit_types(self):
+        return ['Tanks']
 
-        if (self.app.has_strategy("Investor") and
-            (self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
-            not self.data.has_full_investments(days_missing=2)):
-            # if only missing one day of investments, this is normal, don't
-            # sell anything, otherwise sell a chunk
-            return self.investing_sell_ratio
+    def get_sell_unit_types(self):
 
-        # in general, we will sell a small portion of our army to suppliment
-        #   region growth
-        return self.normal_sell_ratio
-
-    def get_army_buy_ratio(self):
-
-        # if we are going to try to trade, don't buy anything
-        will_attempt_to_trade = False
-        if self.app.has_strategy("LocalLackey"):
-            lackey = self.app.get_strategy(
-                    "LocalLackey")
-            will_attempt_to_trade = lackey.check_can_trade(
-                    lackey.get_trade_ratio())
-
-        if self.app.has_strategy("Lackey"):
-            lackey = self.app.get_strategy(
-                    "Lackey")
-            will_attempt_to_trade = lackey.check_can_trade(
-                    lackey.get_trade_ratio())
-
-        if will_attempt_to_trade:
-            botlog.info("We may trade this round, do not buyi anything")
-            return 0
-
-
-        if not self.data.is_oop():
-            return self.protection_buy_ratio
-
-        if (self.app.has_strategy("Investor") and
-                    (
-                            self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
-                not self.data.has_full_investments(days_missing=2)):
-            # if only missing one day of investments, this is normal, don't
-            # buy anything, otherwise buy a chunk
-            return self.investing_buy_ratio
-
-        # in general, we will buy a to suppliment
-        #   industry
-        return self.normal_buy_ratio
-
-
-    def sell(self, sellItems, sellRatio):
-
-        # we start at the buy menu
-        in_buy = True
-
-        if self.visited_sell_menu and sellRatio <= 0:
-            return
-
-        # sell all the items specified
-        for saleItem in sellItems:
-            if str(saleItem) != '6':
-
-                ammount = self.data.get_number(saleItem)
-
-                if ammount is None:
-                    botlog.warn('can not sell item, ' + 
-                            'ammount has not been parsed')
-                    continue
-
-                if ammount == 0:
-                    continue
-
-                ammount = int(round(ammount * sellRatio, 0))
-
-                if in_buy:
-                    # sell all tanks and return to buy menu
-                    self.app.send('s')
-                    self.visited_sell_menu = True
-                    # perform a read and through a spending state to parse all the data
-                    self.sp.parse(self.app, self.app.read())
-                    in_buy = False
-
-                if ammount > 0:
-
-                    # sell the item
-                    self.app.send(saleItem,
-                                  comment="selling item " + str(saleItem))
-
-                    # read max ammount for sale
-                    self.app.metadata.max_ammount = -1
-                    self.sp.parse(self.app, self.app.read())
-                    if self.app.metadata.max_ammount == -1:
-                        raise Exception("Unable to read max sale amount")
-
-                    botlog.info("Max sale amount is " +
-                                str(self.app.metadata.max_ammount) +
-                                " desired sale ammount is " +
-                                str(ammount) + ", t1 is " +
-                                str(type(self.app.metadata.max_ammount)) +
-                                " and t2 is " + str(type(ammount)) +
-                                ", too much? " +
-                                str(self.app.metadata.max_ammount <
-                                    ammount))
-
-                    # if max ammoutn for sale is less than what we are selling
-                    if self.app.metadata.max_ammount < ammount:
-                        # cap off how much we are selling
-                        ammount = self.app.metadata.max_ammount
-
-                    # send the number we are selling
-                    self.app.sendl(ammount, comment="Selling this many")
-                    self.sp.parse(self.app, self.app.read())
-
-            else:
-                raise Exception("Do not know how to drop regions yet")
-
-        # return to buy menu
-        if not in_buy:
-            self.app.send('b')
-            self.sp.parse(self.app, self.app.read())
-
-
-    def on_spending_menu(self):
-
-        # specialize if we have not hyet
-        if self.do_specialize:
-            self.app.send_seq(['*', 3, 5, 0],
-                              comment="Specializing industry on tanks")
-            self.do_specialize = False
-
-        # Sell items
-
-        sell_ratio = self.get_army_sell_ratio()
-
-        # in protection sell everything, otherwise just tanks
         if self.data.is_oop():
-            sellItems = [Tanks.menu_option]
+            sellItems = ["Tanks"]
         else:
             sellItems = [
-                Troopers.menu_option,
-                Turrets.menu_option,
-                Jets.menu_option,
-                Tanks.menu_option,
-                Bombers.menu_option,
-                Carriers.menu_option
+                "Troopers",
+                "Turrets",
+                "Jets",
+                "Tanks",
+                "Bombers",
+                "Carriers"
             ]
-
-        botlog.info("Selling " + str(round(sell_ratio * 100, 1)) +
-                    "% of " + str(sellItems))
-
-        self.sell(sellItems, sell_ratio)
-        self.sp.parse(self.app, self.app.read())
-
-        # buy tanks
-        buy_ratio = self.get_army_buy_ratio()
-        if buy_ratio > 0:
-            self.buy_army_units("Tanks", buy_ratio)
-
-        # enter region buying menu
-        RegionBuy(self.app, enter_region_menu=True)
-
-        # we should still be at buy menu
+        return sellItems
 
--- bbot/MainStrategy.py
+++ bbot/MainStrategy.py
@@ -1,25 +1,55 @@
-__author__ = 'dhkarimi'
+#!/usr/bin/env python
+
+# Author: Derrick H. Karimi
+# Copyright 2014
+# Unclassified
 
 from bbot.Utils import *
 from bbot.Strategy import Strategy
+from bbot.SpendingParser import SpendingParser
+from bbot.Data import *
+from bbot.RegionBuy import RegionBuy
+
+S = SPACE_REGEX
+N = NUM_REGEX
+
+
+def get_region_ratio(app, context):
+    return app.metadata.default_region_ratio(app, context)
+
 
 class MainStrategy(Strategy):
     def __init__(self, app):
         Strategy.__init__(self, app)
-
-        self.protection_sell_ratio = self.get_app_value(
+        self.data = self.app.data
+        self.app.metadata.get_region_ratio_func = get_region_ratio
+        self.app.metadata.get_region_ratio_context = self
+        self.sp = SpendingParser()
+        self.do_specialize = False
+        self.protection_sell_ratio = self.app.get_app_value(
             "protection_sell_ratio")
-        self.investing_sell_ratio = self.get_app_value(
+        self.investing_sell_ratio = self.app.get_app_value(
             "investing_sell_ratio")
-        self.normal_sell_ratio = self.get_app_value(
+        self.normal_sell_ratio = self.app.get_app_value(
             "normal_sell_ratio")
 
-        self.protection_buy_ratio = self.get_app_value(
+        self.protection_buy_ratio = self.app.get_app_value(
             "protection_buy_ratio")
-        self.investing_buy_ratio = self.get_app_value(
+        self.investing_buy_ratio = self.app.get_app_value(
             "investing_buy_ratio")
-        self.normal_buy_ratio = self.get_app_value(
+        self.normal_buy_ratio = self.app.get_app_value(
             "normal_buy_ratio")
+        self.visited_sell_menu = False
+
+        if self.normal_sell_ratio * self.normal_buy_ratio != 0:
+            botlog.warn("buy ratio of " + str(self.normal_buy_ratio) +
+                        " and sell ratio of " + str(self.normal_sell_ratio) +
+                        " are self conflicting")
+
+    def get_priority(self):
+        return MED_PRIORITY
+
+
 
     def get_army_sell_ratio(self):
 
@@ -30,7 +60,8 @@ class MainStrategy(Strategy):
             return self.protection_sell_ratio
 
         if (self.app.has_strategy("Investor") and
-                    (self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
+                    (
+                            self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
                 not self.data.has_full_investments(days_missing=2)):
             # if only missing one day of investments, this is normal, don't
             # sell anything, otherwise sell a chunk
@@ -40,8 +71,117 @@ class MainStrategy(Strategy):
         #   region growth
         return self.normal_sell_ratio
 
-    def buy_army_units(self, unit_types, buyratio=None,
-                       desired_ammount=None):
+    def get_army_buy_ratio(self):
+
+        # if we are going to try to trade, don't buy anything
+        will_attempt_to_trade = False
+        if self.app.has_strategy("LocalLackey"):
+            lackey = self.app.get_strategy(
+                "LocalLackey")
+            will_attempt_to_trade = lackey.check_can_trade(
+                lackey.get_trade_ratio())
+
+        if self.app.has_strategy("Lackey"):
+            lackey = self.app.get_strategy(
+                "Lackey")
+            will_attempt_to_trade = lackey.check_can_trade(
+                lackey.get_trade_ratio())
+
+        if will_attempt_to_trade:
+            botlog.info("We may trade this round, do not buyi anything")
+            return 0
+
+        if not self.data.is_oop():
+            return self.protection_buy_ratio
+
+        if (self.app.has_strategy("Investor") and
+                    (
+                            self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
+                not self.data.has_full_investments(days_missing=2)):
+            # if only missing one day of investments, this is normal, don't
+            # buy anything, otherwise buy a chunk
+            return self.investing_buy_ratio
+
+        # in general, we will buy a to suppliment
+        # industry
+        return self.normal_buy_ratio
+
+
+    def sell(self, sellItems, sellRatio):
+
+        if isinstance(sellItems, basestring):
+            sellItems = make_string_list(sellItems)
+
+        # we start at the buy menu
+        in_buy = True
+
+        if self.visited_sell_menu and sellRatio <= 0:
+            return
+
+        # sell all the items specified
+        for saleItem in sellItems:
+            if str(saleItem) != '6':
+
+                ammount = self.data.get_number(saleItem)
+
+                if ammount is None:
+                    botlog.warn('can not sell item, ' +
+                                'ammount has not been parsed')
+                    continue
+
+                if ammount == 0:
+                    continue
+
+                ammount = int(round(ammount * sellRatio, 0))
+
+                if in_buy:
+                    # sell all tanks and return to buy menu
+                    self.app.send('s')
+                    self.visited_sell_menu = True
+                    # perform a read and through a spending state to parse all the data
+                    self.sp.parse(self.app, self.app.read())
+                    in_buy = False
+
+                if ammount > 0:
+
+                    # sell the item
+                    self.app.send(saleItem,
+                                  comment="selling item " + str(saleItem))
+
+                    # read max ammount for sale
+                    self.app.metadata.max_ammount = -1
+                    self.sp.parse(self.app, self.app.read())
+                    if self.app.metadata.max_ammount == -1:
+                        raise Exception("Unable to read max sale amount")
+
+                    botlog.info("Max sale amount is " +
+                                str(self.app.metadata.max_ammount) +
+                                " desired sale ammount is " +
+                                str(ammount) + ", t1 is " +
+                                str(type(self.app.metadata.max_ammount)) +
+                                " and t2 is " + str(type(ammount)) +
+                                ", too much? " +
+                                str(self.app.metadata.max_ammount <
+                                    ammount))
+
+                    # if max ammoutn for sale is less than what we are selling
+                    if self.app.metadata.max_ammount < ammount:
+                        # cap off how much we are selling
+                        ammount = self.app.metadata.max_ammount
+
+                    # send the number we are selling
+                    self.app.sendl(ammount, comment="Selling this many")
+                    self.sp.parse(self.app, self.app.read())
+
+            else:
+                raise Exception("Do not know how to drop regions yet")
+
+        # return to buy menu
+        if not in_buy:
+            self.app.send('b')
+            self.sp.parse(self.app, self.app.read())
+
+    def buy_army_units(self, unit_types, buyratio=None, desired_ammount=None):
 
         if ((buyratio is None and desired_ammount is None) or
                 (buyratio is not None and desired_ammount is not None)):
@@ -51,12 +191,10 @@ class MainStrategy(Strategy):
         botlog.debug("Requested buy with desired_ammount: " + str(
             desired_ammount))
 
-        unit_types = Utils.make_string_list(unit_types)
-
         sp = SpendingParser()
 
         if isinstance(unit_types, basestring):
-            unit_types = [unit_types]
+            unit_types = make_string_list(unit_types)
 
         if unit_types is None or len(unit_types) == 0:
             raise Exception("No unit_types provided")
@@ -73,9 +211,8 @@ class MainStrategy(Strategy):
                 gold = self.data.realm.gold * buyratio
                 ammount = gold / price
             elif ammount is None:
-                raise Exception(
-                    "Did not specify ratio or ammount of " + str(
-                        unit_type) + " to buy")
+                raise Exception("Did not specify ratio or ammount of " + str(
+                    unit_type) + " to buy")
 
             ammount = int(math.floor(ammount))
 
@@ -105,179 +242,65 @@ class MainStrategy(Strategy):
 
         return tot_ammount
 
+    def get_specialize_sequence(self):
+        return None
 
-    def get_army_buy_ratio(self):
+    def get_sell_unit_types(self):
+        if self.app.has_app_value("sell_unit_types"):
+            return make_string_list(
+                self.app.get_app_value("sell_unit_types"))
+        return None
 
-        # if we are going to try to trade, don't buy anything
-        will_attempt_to_trade = False
-        if self.app.has_strategy("LocalLackey"):
-            lackey = self.app.get_strategy(
-                "LocalLackey")
-            will_attempt_to_trade = lackey.check_can_trade(
-                lackey.get_trade_ratio())
+    def get_buy_unit_types(self):
+        if self.app.has_app_value("buy_unit_types"):
+            return make_string_list(
+                self.app.get_app_value("buy_unit_types"))
+        return None
 
-        if self.app.has_strategy("Lackey"):
-            lackey = self.app.get_strategy(
-                "Lackey")
-            will_attempt_to_trade = lackey.check_can_trade(
-                lackey.get_trade_ratio())
+    def on_spending_menu(self):
 
-        if will_attempt_to_trade:
-            botlog.info("We may trade this round, do not buying anything")
-            return 0
+        # specialize if we have not hyet
+        if self.do_specialize:
+            seq = self.get_specialize_sequence()
+            if seq  is not None:
+                self.app.send_seq(seq,
+                              comment="Specializing industry on tanks")
+                buf = self.app.read()
+                self.sp.parse(buf)
+            self.do_specialize = False
 
-        if not self.data.is_oop():
-            return self.protection_buy_ratio
+        # Sell items
 
-        if (self.app.has_strategy("Investor") and (
-                self.data.realm.bank.investments) > 0 and  # TODO a better way to learn if investments are unparsed, or try to garantee that they are parsed before calling this funciton
-                not self.data.has_full_investments(days_missing=2)):
-            # if only missing one day of investments, this is normal, don't
-            # buy anything, otherwise buy a chunk
-            return self.investing_buy_ratio
+        sell_ratio = self.get_army_sell_ratio()
 
-        # in general, we will buy a to suppliment
-        # industry
-        return self.normal_buy_ratio
+        sellunits = self.get_sell_unit_types()
+        sellItems = []
 
-
-    def sell(self, sellItems, sellRatio):
-        # we start at the buy menu
-        in_buy = True
-
-        if self.visited_sell_menu and sellRatio <= 0:
-            return
-
-        # sell all the items specified
-        for saleItem in sellItems:
-            if str(saleItem) != '6':
-
-                ammount = self.data.get_number(saleItem)
-
-                if ammount is None:
-                    botlog.warn('can not sell item, ' +
-                                'ammount has not been parsed')
-                    continue
-
-                if ammount == 0:
-                    continue
-
-                ammount = int(round(ammount * sellRatio, 0))
-
-                if in_buy:
-                    # sell all tanks and return to buy menu
-                    self.app.send('s')
-                    self.visited_sell_menu = True
-                    # perform a read and through a spending state to parse all the data
-                    self.sp.parse(self.app, self.app.read())
-                    in_buy = False
-
-                if ammount > 0:
-
-                    # sell the item
-                    self.app.send(saleItem,
-                                  comment="selling item " + str(saleItem))
-
-                    # read max ammount for sale
-                    self.app.metadata.max_ammount = -1
-                    self.sp.parse(self.app, self.app.read())
-                    if self.app.metadata.max_ammount == -1:
-                        raise Exception("Unable to read max sale amount")
-
-                    botlog.info("Max sale amount is " +
-                                str(self.app.metadata.max_ammount) +
-                                " desired sale ammount is " +
-                                str(ammount) + ", t1 is " +
-                                str(type(self.app.metadata.max_ammount)) +
-                                " and t2 is " + str(type(ammount)) +
-                                ", too much? " +
-                                str(self.app.metadata.max_ammount <
-                                    ammount))
-
-                    # if max ammoutn for sale is less than what we are selling
-                    if self.app.metadata.max_ammount < ammount:
-                        # cap off how much we are selling
-                        ammount = self.app.metadata.max_ammount
-
-                    # send the number we are selling
-                    self.app.sendl(ammount, comment="Selling this many")
-                    self.sp.parse(self.app, self.app.read())
-
+        for s in sellunits:
+            mnuopt = self.app.data.get_menu_option(s)
+            if mnuopt is not None:
+                sellItems.append(mnuopt)
             else:
-                raise Exception("Do not know how to drop regions yet")
+                botlog.warn("could not find menu option for unit: " + str(s))
 
-        # return to buy menu
-        if not in_buy:
-            self.app.send('b')
+        # in protection sell everything, otherwise just tanks
+
+        if sellItems is None and len(sellItems) > 0:
+            botlog.info("Selling " + str(round(sell_ratio * 100, 1)) +
+                        "% of " + str(sellItems))
+
+            self.sell(sellItems, sell_ratio)
             self.sp.parse(self.app, self.app.read())
 
-    def sell(self, sellItems, sellRatio):
+        # buy tanks
+        buy_ratio = self.get_army_buy_ratio()
+        buy_items = self.get_buy_unit_types()
+        if (buy_ratio > 0 and self.data.realm.gold > 0 and
+            buy_items is not None and len(buy_items) > 0):
+            self.buy_army_units(buy_items, buy_ratio)
 
-        # we start at the buy menu
-        in_buy = True
+        # enter region buying menu
+        RegionBuy(self.app, enter_region_menu=True)
 
-        if self.visited_sell_menu and sellRatio <= 0:
-            return
+        # we should still be at buy menu
 
-        # sell all the items specified
-        for saleItem in sellItems:
-            if str(saleItem) != '6':
-
-                ammount = self.data.get_number(saleItem)
-
-                if ammount is None:
-                    botlog.warn('can not sell item, ' +
-                                'ammount has not been parsed')
-                    continue
-
-                if ammount == 0:
-                    continue
-
-                ammount = int(round(ammount * sellRatio, 0))
-
-                if in_buy:
-                    # sell all tanks and return to buy menu
-                    self.app.send('s')
-                    self.visited_sell_menu = True
-                    # perform a read and through a spending state to parse all the data
-                    self.sp.parse(self.app, self.app.read())
-                    in_buy = False
-
-                if ammount > 0:
-
-                    # sell the item
-                    self.app.send(saleItem,
-                                  comment="selling item " + str(saleItem))
-
-                    # read max ammount for sale
-                    self.app.metadata.max_ammount = -1
-                    self.sp.parse(self.app, self.app.read())
-                    if self.app.metadata.max_ammount == -1:
-                        raise Exception("Unable to read max sale amount")
-
-                    botlog.info("Max sale amount is " +
-                                str(self.app.metadata.max_ammount) +
-                                " desired sale ammount is " +
-                                str(ammount) + ", t1 is " +
-                                str(type(self.app.metadata.max_ammount)) +
-                                " and t2 is " + str(type(ammount)) +
-                                ", too much? " +
-                                str(self.app.metadata.max_ammount <
-                                    ammount))
-
-                    # if max ammoutn for sale is less than what we are selling
-                    if self.app.metadata.max_ammount < ammount:
-                        # cap off how much we are selling
-                        ammount = self.app.metadata.max_ammount
-
-                    # send the number we are selling
-                    self.app.sendl(ammount, comment="Selling this many")
-                    self.sp.parse(self.app, self.app.read())
-
-            else:
-                raise Exception("Do not know how to drop regions yet")
-
-        # return to buy menu
-        if not in_buy:
-            self.app.send('b')
-            self.sp.parse(self.app, self.app.read())
